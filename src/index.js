d1e0cd5566dd4b3e89fb479daa96388de6328f41
Bisaso Ronald <bronnie313@gmail.com>
2023-05-23 13:50:10 +0300

    add popup event listerner to the comment button

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 import './style.css';
 
 // get data from Api
-const getData = async() => {
-    const res = await fetch('https://api.tvmaze.com/shows');
-    const data = await res.json()
-    return data;
-}
+const getData = async () => {
+  const res = await fetch('https://api.tvmaze.com/shows');
+  const data = await res.json();
+  return data;
+};
 
-getData()
+getData();
 
 // display pokemon data
 const items = document.getElementById('items');
 
- const populateData = async () => {
-    const data = await getData();
-    const limit = 12;
-    data.forEach((data, i) => {
-        if(i < limit) {
-            items.innerHTML += `
+const populateData = async () => {
+  const data = await getData();
+  const limit = 12;
+  data.forEach((data, i) => {
+    if (i < limit) {
+      items.innerHTML += `
         <div class="poke">
             <img id="image" src=${data.image.medium}>
             <p>${data.name}</p>
             <button class="commentBtn" data-index="${i}">comment</button>
         </div>
         `;
-        }
-    })
-}
+    }
+  });
+};
 
 populateData();
+
+items.addEventListener('click', (event) => {
+    if (event.target.classList.contains('commentBtn')) {
+        const index = parseInt(event.target.dataset.index, 10)
+        console.log(index)
+        
+    }
+})
